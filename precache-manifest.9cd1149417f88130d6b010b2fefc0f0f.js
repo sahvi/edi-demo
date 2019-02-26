dcc7003c181d52a34a6c092d79650d69ac0a945b
tqm657 (Madiraju, Raghuram) <tqm657@f45c89abac8d.cof.ds.capitalone.com>
2019-02-25 20:42:18 -0600

    initial demo version

---
+++ precache-manifest.9cd1149417f88130d6b010b2fefc0f0f.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "081bf019322fc100b3b84f34d9bff302",
+    "url": "/static/media/product1.081bf019.jpg"
+  },
+  {
+    "revision": "260acac6742f3e4c829d",
+    "url": "/static/css/main.793efa24.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f34e943fbffb73c68e761828b1c00980",
+    "url": "/static/media/sidebar-4.f34e943f.jpg"
+  },
+  {
+    "revision": "45782c4b72fd0eb3a43a",
+    "url": "/static/js/2.45782c4b.chunk.js"
+  },
+  {
+    "revision": "ae10829f019b0103525a6cd1102ee213",
+    "url": "/static/media/card-2.ae10829f.jpeg"
+  },
+  {
+    "revision": "6ee66b2cb8a3d9988e37b83b3b4a590e",
+    "url": "/static/media/card-1.6ee66b2c.jpeg"
+  },
+  {
+    "revision": "7a6bc10028a79f78e424a9c71499fa77",
+    "url": "/static/media/avatar.7a6bc100.jpg"
+  },
+  {
+    "revision": "3122abf4a3e1067926c08dee7684522d",
+    "url": "/static/media/card-profile1-square.3122abf4.jpg"
+  },
+  {
+    "revision": "d30c9e30978c9278180dcce2bb66c054",
+    "url": "/static/media/sidebar-2.d30c9e30.jpg"
+  },
+  {
+    "revision": "ebe9884b2152a08a525fcd5054019532",
+    "url": "/static/media/image_placeholder.ebe9884b.jpg"
+  },
+  {
+    "revision": "1eb7fff2a469da3cc6c3311572d7696d",
+    "url": "/static/media/placeholder.1eb7fff2.jpg"
+  },
+  {
+    "revision": "4ae4eeff542ab32204e8409d170e14a0",
+    "url": "/static/media/card-3.4ae4eeff.jpeg"
+  },
+  {
+    "revision": "260acac6742f3e4c829d",
+    "url": "/static/js/main.260acac6.chunk.js"
+  },
+  {
+    "revision": "a0e400118b382fc007111de984da7552",
+    "url": "/static/media/product3.a0e40011.jpg"
+  },
+  {
+    "revision": "aba54d652e5df3b9f1f8024cbeb6630f",
+    "url": "/static/media/marc.aba54d65.jpg"
+  },
+  {
+    "revision": "49b54380ba092ba68ab160f3dd8d5773",
+    "url": "/static/media/register.49b54380.jpeg"
+  },
+  {
+    "revision": "59fc83e26c663ade5dcae6e7d56ca970",
+    "url": "/static/media/login.59fc83e2.jpeg"
+  },
+  {
+    "revision": "35f7d126c07a40a893f3f6cc8d1c3717",
+    "url": "/static/media/lock.35f7d126.jpeg"
+  },
+  {
+    "revision": "5b7c1cad66a2efdbf3604615cc9d7d1c",
+    "url": "/static/media/clint-mckoy.5b7c1cad.jpg"
+  },
+  {
+    "revision": "4d2f2f4baa617ba08685b4d9118dc029",
+    "url": "/static/media/bg-pricing.4d2f2f4b.jpeg"
+  },
+  {
+    "revision": "eec7c7f60134e712ef7174c96ca7ee5a",
+    "url": "/static/media/logo-white.eec7c7f6.svg"
+  },
+  {
+    "revision": "61c4c50e61d68de31c8951af5470a2c2",
+    "url": "/static/media/sidebar-1.61c4c50e.jpg"
+  },
+  {
+    "revision": "c963330bcd48670a75ffdd38622b1532",
+    "url": "/static/media/sidebar-3.c963330b.jpg"
+  },
+  {
+    "revision": "45782c4b72fd0eb3a43a",
+    "url": "/static/css/2.9b52b13d.chunk.css"
+  },
+  {
+    "revision": "e371210a21364958b87b66e4a6955ffb",
+    "url": "/index.html"
+  }
+];
