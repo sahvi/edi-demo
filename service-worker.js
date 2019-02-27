dcf93ef7befd354eaeb8559327beac8eeea16b28
tqm657 (Madiraju, Raghuram) <tqm657@f45c89abac8d.cof.ds.capitalone.com>
2019-02-26 23:51:00 -0600

    new changes

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9cd1149417f88130d6b010b2fefc0f0f.js"
+  "/edi-demo/precache-manifest.76d4c911ba8b7ba713990f90641b6b0b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/edi-demo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
